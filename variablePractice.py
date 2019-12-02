e0f25e62be20fa0a2e3f497f5276e4f3e6bc6db1
ccaguirre <57292005+ccaguirre@users.noreply.github.com>
2019-12-01 18:55:50 -0600

    Update variablePractice.py

---
+++ variablePractice.py
@@ -0,0 +1,61 @@
+'''
+Created on Dec 1, 2019
+
+
+@author: ITAUser
+'''
+
+
+var1 = 5
+print(var1)
+var2 = 10
+print(var2)
+var3 = var2
+print(var3)
+x = 24
+print(x)
+y = 100
+print (y)
+z = 1.1
+print(z)
+a = 2.75
+print(a)
+b = a 
+print(b)
+c = 100.1
+print(c)
+d = 12.3
+print(d)
+name = "Caleb"
+print(name)
+e = "c"
+print(e)
+f = "e"
+print(f)
+g = "chicken"
+print(g)
+h = "cal"
+print(h)
+done=False
+print(done)
+smart = True
+print(smart)
+i = done
+print(done)
+j = False
+print(j)
+almostdone=True
+print(almostdone)
+list_int = [x,y,var1, var2,var3]
+print(list_int.index(var1))
+listString = [name,e,f,g,h]
+print(listString.index(g))
+listBoolean=[done,smart,i,j,almostdone]
+print(listBoolean.index(done))
+print(listBoolean.index(smart))
+print(listBoolean.index(i))
+print(listBoolean.index(j))
+print(listBoolean.index(almostdone))
+
+
+
